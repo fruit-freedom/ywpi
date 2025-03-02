bf2879376c0507c835527e7641fd3ea790ca8c18
fruit-freedom <newtworktransfer@gmail.com>
2025-03-02 22:50:41 +0300

    WEB: Remove unused code

--- web/src/pages/ProjectPage/nodes/WithDragHandle.tsx
+++ web/src/pages/ProjectPage/nodes/WithDragHandle.tsx
@@ -1,9 +1,6 @@
-import { useState, ReactNode } from "react";
+import { ReactNode } from "react";
 
-import { Box, Button, IconButton, Menu, MenuItem, Modal, Stack, Typography } from "@mui/material"
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { MethodWithAgentId, useMethods } from "../store/methods";
-import MethodCard, { BorrowedFields } from "../../../components/MethodCard/MethodCard";
+import { IconButton, Stack } from "@mui/material"
 import BlurOnIcon from '@mui/icons-material/BlurOn';
 import { NodeMenu } from "../components/NodeMenu";
 
@@ -13,70 +10,7 @@ interface WithDragHandle {
     data?: any;
 }
 
-interface ChosenState {
-    method?: MethodWithAgentId;
-    defaultValues?: any;
-    open: boolean;
-    borrowedFields?: BorrowedFields;
-}
-
 export const WithDragHandle = ({ children, tp, data }: WithDragHandle) => {
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-
-    const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = (option?: MethodWithAgentId) => {
-        setAnchorEl(null);
-
-
-        /*
-            There are convertation logic from ywpi object data
-            to method card `defaultvalues`.
-            This convertation logic should use type matching.
-        */
-
-        const borrowedFields: BorrowedFields = {};
-
-        if (option) {
-            const textInputs = option.inputs.filter(e => e.type == 'text');
-            const defaultValues = textInputs.reduce((acc, item) => {
-
-                // Each object `tp` should provide method like `toFormValue`
-                acc[item.name] = data.text;
-
-                borrowedFields[item.name] = {
-                    objectId: data.objectId, // TODO: get object id from node
-                    path: 'text'
-                }
-
-                return acc;
-            }, {} as any)
-
-            console.log('borrowedFields', borrowedFields)
-
-            setChosen({
-                method: option,
-                defaultValues,
-                borrowedFields,
-                open: true
-            })
-        }
-    };
-
-    const [chosen, setChosen] = useState<ChosenState>({
-        method: undefined,
-        open: false
-    });
-
-
-
-    // Think how to optimize re renders
-    const { methods } = useMethods();
-    const options = methods.get(tp);
-
     const showAllRelated = () => {
         fetch(`/api/objects/${data.objectId}/related`)
         .then(e => e.json())
